bd3347108439178f19413f1c1d4fd1b3127967bd
Olivier Dolbeau <contact@odolbeau.fr>
2014-10-25 00:26:12 +0200

    Complete Makefile

--- Makefile
+++ Makefile
@@ -1,8 +1,10 @@
-pecl:
+extension:
+	./prepare_rabbit.sh
 	php ./send_messages.php $(filter-out $@,$(MAKECMDGOALS))
 	php ./bench_consume.php pecl $(filter-out $@,$(MAKECMDGOALS))
 
-php-amqplib:
+lib:
+	./prepare_rabbit.sh
 	php ./send_messages.php $(filter-out $@,$(MAKECMDGOALS))
 	php ./bench_consume.php amqplib $(filter-out $@,$(MAKECMDGOALS))
 
--- prepare_rabbit.sh
+++ prepare_rabbit.sh
@@ -9,9 +9,3 @@ echo "# Declaring mapping"
 rabbitmqadmin declare exchange name=bench type=direct auto_delete=false durable=true --vhost=bench
 rabbitmqadmin declare queue name=bench auto_delete=false durable=true --vhost=bench
 rabbitmqadmin declare binding source=bench routing_key=bench destination=bench --vhost=bench
-
-echo "# Create some messages"
-for i in `seq 1 100000`
-do
-    rabbitmqadmin publish routing_key="bench" payload="message$i" exchange="bench" --vhost=bench
-done
